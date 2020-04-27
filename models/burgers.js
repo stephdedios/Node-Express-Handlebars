5e3c2165b28ea9920d0e41cfe64301bf741ca11b
Stephanie DeDios <stephtdedios@gmail.com>
2020-04-27 13:11:41 -0700

    tried fixing stylesheet connection

--- models/burgers.js
+++ models/burgers.js
@@ -1,55 +1,29 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function () {
-  $(".change-state").on("click", function (event) {
-    var id = $(this).data("id");
-    var newState = $(this).data("newstate");
+// Import the ORM to create functions that will interact with the database.
+var orm = require("../config/orm.js");
 
-    var newDevouredState = {
-      devoured: true,
-    };
-
-    // Send the PUT request.
-    $.ajax("/api/burgers/" + id, {
-      type: "PUT",
-      data: newDevouredState,
-    }).then(function () {
-      console.log("changed devoured to", newState);
-      // Reload the page to get the updated list
-      location.reload();
+var burger = {
+  selectAll: function (cb) {
+    orm.selectAll("burgers", function (res) {
+      cb(res);
     });
-  });
-
-  $(".create-form").on("submit", function (event) {
-    // Make sure to preventDefault on a submit event.
-    event.preventDefault();
-
-    var newBurger = {
-      burger: $("#ca").val().trim(),
-      // devoured: true
-    };
-
-    console.log(newBurger);
-    // Send the POST request.
-    $.ajax("/api/burgers/", {
-      type: "POST",
-      data: newBurger,
-    }).then(function () {
-      console.log("created new Burger");
-      // Reload the page to get the updated list
-      location.reload();
+  },
+  // The variables cols and vals are arrays.
+  insertOne: function (cols, vals, cb) {
+    orm.insertOne("burgers", cols, vals, function (res) {
+      cb(res);
     });
-  });
-
-  $(".delete-burger").on("click", function (event) {
-    var id = $(this).data("id");
-
-    // Send the DELETE request.
-    $.ajax("/api/burgers/" + id, {
-      type: "DELETE",
-    }).then(function () {
-      console.log("deleted burger", id);
-      // Reload the page to get the updated list
-      location.reload();
+  },
+  updateOne: function (objColVals, condition, cb) {
+    orm.updateOne("burgers", objColVals, condition, function (res) {
+      cb(res);
     });
-  });
-});
+  },
+  delete: function (condition, cb) {
+    orm.delete("burgers", condition, function (res) {
+      cb(res);
+    });
+  },
+};
+
+// Export the database functions for the controller (burgers_controller.js).
+module.exports = burger;
